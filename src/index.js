f02bc0b3a6906b574f6ed948032a482a9500e13a
Wesley Tsang <tsangwe@oocl.com>
2020-04-25 18:34:40 +0800

    [Wesley] Applied prettier at index.js

--- src/index.js
+++ src/index.js
@@ -7,32 +7,32 @@ import { Button, Layout, Space } from "antd";
 import HomePageContainer from "./components/HomePageContainer";
 import SearchPageContainer from "./components/SearchPageContainer";
 import { CarOutlined } from "@ant-design/icons";
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 const { Header, Content, Footer } = Layout;
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-    <Layout className="layout">
-      <Header className="layoutHeader">
-        <CarOutlined className="headerLogo" />
-        <div className="loginAndRegister">
-          <Space>
-            <Button type="primary">Login</Button>
-            <Button>Register</Button>
-          </Space>
-        </div>
-      </Header>
-      <Content className="layoutContent">
-        <Route exact={true} path="/" component={HomePageContainer} />
-        <Route path="/search" component={SearchPageContainer} />
-        {/* <Route path="/user-profile" component={} /> */}
-      </Content>
-      <Footer id="layoutFooter" style={{ textAlign: "center" }}>
-        All the right reserved by 500-YourThingIsBroken @copyright
-      </Footer>
-    </Layout>
+      <Layout className="layout">
+        <Header className="layoutHeader">
+          <CarOutlined className="headerLogo" />
+          <div className="loginAndRegister">
+            <Space>
+              <Button type="primary">Login</Button>
+              <Button>Register</Button>
+            </Space>
+          </div>
+        </Header>
+        <Content className="layoutContent">
+          <Route exact={true} path="/" component={HomePageContainer} />
+          <Route path="/search" component={SearchPageContainer} />
+          {/* <Route path="/user-profile" component={} /> */}
+        </Content>
+        <Footer id="layoutFooter" style={{ textAlign: "center" }}>
+          All the right reserved by 500-YourThingIsBroken @copyright
+        </Footer>
+      </Layout>
     </Router>
   </React.StrictMode>,
   document.getElementById("root")
